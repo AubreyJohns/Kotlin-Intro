9be8c89a2993482dc00bcb0c7d1e27e63dadd0ae
AubreyJohns <aubreyomondi@gmail.com>
2019-08-27 04:02:35 +0300

    Updated Fortune Cookie challenge

--- src/fortuneCookie.kt
+++ src/fortuneCookie.kt
@@ -10,7 +10,18 @@ fun getFortuneCookie(): String {
         "Today is a good day for exercising restraint.",
         "Take it easy and enjoy life!",
         "Treasure your friends because they are your greatest fortune.")
-    print("Enter your birthday:")
+    print("Enter your birthday: (day only e.g 9)    ")
     val  birthday = (readLine().toString())?.toIntOrNull() ?: 1
-    return fortunes[birthday%7]
+    if (fortunes[birthday%7] != "Take it easy and enjoy life!"){
+        for(fortune in fortunes){
+            print("Try your luck till you get: Take it easy and enjoy life!\n")
+            print("Enter your birthday: ")
+            val  birthday = (readLine().toString())?.toIntOrNull() ?: 1
+            if (fortunes[birthday%7] == "Take it easy and enjoy life!") {4
+                println("Your fortune is: Take it easy and enjoy life!")
+                break
+            }
+            }
+        }
+    return fortunes[birthday % 7]
 }
